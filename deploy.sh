502b524ce72daf397df9a4edb0a12603f4195d3e
file098 <filippodigennaro98@outlook.it>
2023-11-07 18:07:22 +0100

    redesigned ui

--- deploy.sh
+++ deploy.sh
@@ -16,9 +16,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:file098/file098.github.io.git main
-
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 git push -f https://github.com/file098/contact-dad.git main:gh-pages
 
--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -10,74 +10,86 @@
                 Guida ciclista
             </div>
         </div>
-        <div class="subcard">
-            <div class="subcard-text">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam
-                praesentium tenetur et pariatur. Ipsum voluptatum quisquam labore ipsam
-                sunt vel sed illo dolor optio ratione officiis voluptas mollitia, dolore
-                eos.
-            </div>
-            <button class="contact-me">Contattami</button>
-        </div>
     </div>
+    <div class="description">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam
+        praesentium tenetur et pariatur. Ipsum voluptatum quisquam labore ipsam
+        sunt vel sed illo dolor optio ratione officiis voluptas mollitia, dolore
+        eos.
+    </div>
+    <button class="contact-me">Contattami</button>
+    <!-- <iframe class="komoot-iframe" src="https://www.komoot.com/user/3227558101012/embed?stats=1" width="640" height="580" frameborder="0" scrolling="no"></iframe> -->
 </template>
-
 <script>
 export default {
     name: "MainPage",
 };
 </script>
 
-<style>
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap');
+
+body {
+    font-family: 'Poppins', sans-serif;
+}
+
 .card {
-    height: 98vh;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-}
-
-.header {
-    margin-top: 2rem;
-    display: flex;
-    flex-flow: column;
-}
-
-.card-title {
-    font-size: 2.5rem;
-    font-weight: 400;
-    text-transform: uppercase;
-}
-
-.subcard {
     position: relative;
-    height: 70%;
+    height: 60vh;
     display: flex;
     flex-direction: column;
-    justify-content: space-around;
-    align-items: center;
-    margin: 0 2px;
-    box-shadow: 0 0 10px rgba(63, 63, 63, 0.1);
-    border-radius: 30px;
+    justify-content: end;
 
-    background-image: linear-gradient(rgba(0, 0, 0, 0.3),rgba(0, 0, 0, 0.3)) , url("/src/assets/card-bg.jpeg");
+    color: #fff;
+    background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url("/src/assets/card-bg.jpeg");
     background-size: cover;
     background-repeat: no-repeat;
     background-position: center center;
+
+    box-shadow: 0 4px 10px rgba(0, 0, 0, 0.50);
+    border-radius: 50px;
+
+    .header {
+        margin-top: 2rem;
+        display: flex;
+        flex-flow: column;
+        text-align: start;
+        margin: 2rem;
+
+        .card-title {
+            font: 550 48px Poppins;
+            text-transform: uppercase;
+        }
+
+        .card-subtitle {
+            font: 400 16px Poppins;
+        }
+    }
 }
 
-.subcard-text {
-    font: 550 18px Avenir;
+.description {
+    font: 1.2rem Poppins;
+    background-color: #fff;
+    padding: 20px;
     text-align: start;
-    color: #fff;
-    padding: 30px;
+}
+
+.komoot-iframe {
+    padding: 1rem;
+    border-radius: 10px;
+    max-width: 90vw;
 }
 
 .contact-me {
-    font-size: 1.2rem;
+    font: normal 24px Poppins, sans-serif;
     width: 50%;
-    background-color: #fff;
-    border-radius: 10px;
+    border-radius: 26px;
     padding: 20px;
-    margin: 2rem 0;
+    margin: 0 auto;
+    box-shadow: 0 4px 10px rgba(0, 0, 0, 0.33);
+    background: linear-gradient(180deg, rgba(142,205,60,1) 0%, rgba(108,171,39,1) 100%);
+    color: white;
+    border: none;
+    cursor: pointer;
 }
 </style>
